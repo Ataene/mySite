2711a640bc8f062f2a72256a93cecf3a57a385b3
Alaf <ke.alafonye@yahoo.com>
2022-08-16 09:24:45 -0600

    Working Mobile

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import Paper from "@mui/material/Paper";
 import { Container, Typography, Box, Grid } from "@mui/material";
 import myImage from "../images/Alaf.jpg";
-import myImage1 from "../images/logo.png";
-import myImage2 from "../images/logo2.png";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { styled } from "@mui/material/styles";
@@ -12,26 +9,31 @@ import "aos/dist/aos.css";
 AOS.init();
 
 const About = () => {
-  const Item = styled(Paper)(({ theme }) => ({
+  const Item = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: "center",
     color: theme.palette.text.secondary,
   }));
+
   return (
     <>
-      <Container sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Item sx={{ display: { xs: '', sm: 'block', md: 'none' }}}>
-              <img
+      <Container sx={{ flexGrow: 1, marginTop: 8 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6} lg={6}>
+            <Item>
+            <img
                 src={myImage}
                 style={{ borderRadius: 20 }}
                 placeholder="Emmanuel"
                 data-aos="fade-right"
               />
-              <Box>
+            </Item>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4} sx={{marginTop: 5, marginLeft: 0}}>
+            <Item>
+            <Box>
                 <Typography
                   variant="h3"
                   sx={{ marginBottom: 0.2 }}
@@ -54,7 +56,7 @@ const About = () => {
                   BSc In Petroleum Systems Engineering
                 </Typography>
                 <Box
-                  sx={{ display: "flex", flexDirection: "row", marginTop: 2 }}
+                  sx={{ display: "flex", flexDirection: "row", marginTop: 2, justifyContent: "center" }}
                 >
                   <LinkedInIcon
                     onClick={() =>
@@ -84,84 +86,6 @@ const About = () => {
           </Grid>
         </Grid>
       </Container>
-
-      {/* <Container sx={{ marginTop: 10 }}>
-        <Grid
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            alignContent: "center",
-          }}
-        >
-        <Grid item xs={12} md={2}>
-          <Item>
-            <img
-              src={myImage}
-              style={{ borderRadius: 20 }}
-              placeholder="Emmanuel"
-              data-aos="fade-right"
-            />
-          </Item>
-        </Grid>
-          <Grid
-            item
-            xs={12}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              marginTop: "10%",
-              marginLeft: 20,
-            }}
-          >
-            <Typography
-              variant="h3"
-              sx={{ marginBottom: 0.2 }}
-              data-aos="fade-left"
-            >
-              Hello, I'm <span style={{ color: "red" }}>Emmanuel</span> Ala
-            </Typography>
-            <Typography
-              variant="h5"
-              sx={{ marginBottom: 0.5 }}
-              data-aos="fade-left"
-            >
-              A{" "}
-              <span style={{ color: "green" }} data-aos="fade-left">
-                Full Stack Software
-              </span>
-              /Machine Learning Engineer
-            </Typography>
-            <Typography data-aos="fade-left">
-              BSc In Petroleum Systems Engineering
-            </Typography>
-            <Box sx={{ display: "flex", flexDirection: "row", marginTop: 2 }}>
-              <LinkedInIcon
-                onClick={() =>
-                  window.open(
-                    "https://www.linkedin.com/in/alafonye-emmanuel/",
-                    "_blank"
-                  )
-                }
-                sx={{
-                  color: "#0072b1",
-                  fontSize: 40,
-                  cursor: "pointer",
-                  marginRight: 2,
-                }}
-                data-aos="fade-left"
-              />
-              <GitHubIcon
-                onClick={() =>
-                  window.open("https://github.com/Ataene", "_blank")
-                }
-                sx={{ color: "#0072b1", fontSize: 35, cursor: "pointer" }}
-                data-aos="fade-left"
-              />
-            </Box>
-          </Grid>
-        </Grid>
-      </Container> */}
     </>
   );
 };
--- client/src/Components/Contacts.jsx
+++ client/src/Components/Contacts.jsx
@@ -9,12 +9,12 @@ import {
 } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
-import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import MapsHomeWorkIcon from "@mui/icons-material/MapsHomeWork";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
 
 const Contacts = () => {
-  const Item = styled(Paper)(({ theme }) => ({
+  const Item = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -23,45 +23,43 @@ const Contacts = () => {
   }));
   return (
     <>
-      <Container
-        sx={{
-          width: "100%",
-          maringTop: 30,
-          // backgroundImage: "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
-        }}
-      >
-        <Typography
-          variant="h3"
-          sx={{ display: "flex", justifyContent: "center", color: "green" }}
-
-        >
-          Contacts
-        </Typography>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid xs={6}>
-            <Item data-aos="fade-down">
-              <Typography><MapsHomeWorkIcon/> Calgary, Alberta, Canada</Typography>
-              <Typography><MailOutlineIcon/>  ke.alafonye@yahoo.com</Typography>
-              <Typography><ContactPhoneIcon/> +13062093394</Typography>
+      <Container sx={{ flexGrow: 1 }}>
+        <Typography variant="h3" sx={{display: "flex", justifyContent: "center", color: "green"}} >Contact Us</Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={4} lg={4}>
+            <Item>
+              <Typography>
+                <MapsHomeWorkIcon /> Calgary, Alberta, Canada
+              </Typography>
+              <Typography>
+                <MailOutlineIcon />{" "}
+              </Typography>
+              <Typography>
+                <ContactPhoneIcon />{" "}
+              </Typography>
             </Item>
           </Grid>
-          <Grid xs={6}>
-            <Item data-aos="fade-down">
-              <Box sx={{ display: "flex", flexDirection: "row", width:"100%" }}>
+          <Grid item xs={12} md={8} lg={8}>
+            <Item>
+              <Box
+                sx={{ display: "flex", flexDirection: "row" }}
+              >
                 <TextField
                   id="First Name"
                   label="First Name"
                   variant="outlined"
+                  sx={{width: "50%"}}
                 />
                 <TextField
                   id="Last Name"
                   label="Last Name"
                   variant="outlined"
+                  sx={{width: "50%"}}
                 />
               </Box>
               <Box sx={{ display: "flex", flexDirection: "row" }}>
-                <TextField id="Phone" label="Phone" variant="outlined" />
-                <TextField id="Email" label="Email" variant="outlined" />
+                <TextField id="Phone" label="Phone" variant="outlined" sx={{width: "50%"}}  />
+                <TextField id="Email" label="Email" variant="outlined" sx={{width: "50%"}}/>
               </Box>
               <Box sx={{ display: "flex", flexDirection: "column" }}>
                 <TextField id="Message" label="Message..." multiline rows={4} />
